113339de49a10136e834f0e6116ee4464706b6bd
dasdemirs <serifeedasdemir@gmail.com>
2024-03-17 15:05:54 +0300

    serifeSunday

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,21 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class LoginPage {
+    public LoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath ="//a[@href='/login']")
+    public WebElement loginButtonMainPage;
+    @FindBy(id = "username")
+    public  WebElement userName;
+    @FindBy(id = "password")
+    public WebElement password;
+    @FindBy(xpath = "(//button[@type='button'])[3]")
+    public WebElement loginButton;
 }
