f63de18f8e4608cfa21e8228cbbc89cd0accb95d
AhmadRaminZahid <ahmadraminzahid1120@gmail.com>
2024-03-21 01:15:23 +0430

    gokhanMerged

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks" },
         dryRun =false,
-        tags = "@us_19"
+        tags = "@us_22"
 )
 
 public class Runner {
