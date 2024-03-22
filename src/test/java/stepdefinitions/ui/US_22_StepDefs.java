4926e92d44779e3a30c3ada5a758582dfadbb2cf
AhmadRaminZahid <ahmadraminzahid1120@gmail.com>
2024-03-23 01:13:43 +0430

    hello

--- src/test/java/stepdefinitions/ui/US_22_StepDefs.java
+++ src/test/java/stepdefinitions/ui/US_22_StepDefs.java
@@ -1,5 +1,6 @@
 package stepdefinitions.ui;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -19,10 +20,10 @@ import java.util.List;
 public class US_22_StepDefs {
     LoginPage loginPage = new LoginPage();
     AdminManagementPage adminManagementPage= new AdminManagementPage();
-    public static String Adminssn="119-01-1203";
-    public static String Adminphonenumber="079-012-1123";
-    public static String Adminusername="AdminHekmatAmni";
-
+    public static String Adminssn;
+    public static String Adminphonenumber;
+    public static String Adminusername;
+    Faker faker= new Faker();
 
     @When("Admin enters the first name for new Admin")
     public void adminEntersTheFirstNameForNewAdmin() {
@@ -52,16 +53,19 @@ public class US_22_StepDefs {
 
     @And("Admin enters phone number of new Admin in the format\\(xxx-xxx-xxxx)")
     public void adminEntersPhoneNumberOfNewAdminInTheFormatXxxXxxXxxx() {
+        Adminphonenumber="079-012-"+faker.number().numberBetween(1000,9999);
         BrowserUtils.sendKeysWithTimeout(adminManagementPage.phoneNumber,Adminphonenumber,2);
     }
 
     @And("Admin enters SSN number of new Admin in the format\\(xxx-xx-xxxx)")
     public void adminEntersSSNNumberOfNewAdminInTheFormatXxxXxXxxx() {
+        Adminssn=faker.number().numberBetween(100,999)+"-23-5674";
         BrowserUtils.sendKeysWithTimeout(adminManagementPage.ssn,Adminssn,2);
     }
 
     @And("Admin enters a username for new Admin")
     public void adminEntersAUsernameForNewAdmin() {
+        Adminusername=faker.name().username();
         BrowserUtils.sendKeysWithTimeout(adminManagementPage.username,Adminusername,2);
     }
 
