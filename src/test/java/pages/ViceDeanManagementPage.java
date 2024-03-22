58cd41c8bf955020eca264822e814805cc8c3955
Semam Cicek <semam49@gmail.com>
2024-03-22 11:41:25 +0100

    Semam the last version

--- src/test/java/pages/ViceDeanManagementPage.java
+++ src/test/java/pages/ViceDeanManagementPage.java
@@ -15,7 +15,7 @@ public class ViceDeanManagementPage {
     @FindBy (xpath = "//button[@id='controlled-tab-example-tab-lessonProgram']")
     public WebElement viceDeanLessonProgram;
 
-    @FindBy (xpath = "(//*[@id=\"lessonProgramId\"])[7]")
+    @FindBy (xpath = "(//*[@id=\"lessonProgramId\"])[8]")
     public WebElement viceDeanChooseLesson;
     // (//input[@id='lessonProgramId'])[5]
 
