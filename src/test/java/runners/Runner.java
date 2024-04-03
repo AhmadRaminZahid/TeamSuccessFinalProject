03feafee9664dededd40667f70bc9a55a1189a27
Semam Cicek <semam49@gmail.com>
2024-04-03 14:26:20 +0200

    Last Version Wednesday

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks" },
         dryRun =false,
-        tags = "@us19_20_e2e"
+        tags = "@us12_e2e"
 
 )
 
--- src/test/java/stepdefinitions/api/US_12_StepDefs.java
+++ src/test/java/stepdefinitions/api/US_12_StepDefs.java
@@ -27,14 +27,19 @@ public class US_12_StepDefs {
     ObjectMapper objectMapper = new ObjectMapper();
     Response response;
     Lessonpojo payload;
-    //public static int lessonProgramId;
 
-    //public static Integer lessonProgramId;
+    int unAssignedLessonProgId;
 
-    List<Integer> lessonProgIdList = List.of(4030);
+    @Then("sets the url for getAllUnassignedLessons to find an unassigned lessons")
+    public void setsTheUrlForGetAllUnassignedLessons() {
+        spec.pathParams("first", "lessonPrograms", "second", "getAllUnassigned");
+        response = given(spec).get("{first}/{second}");
 
-    String id;
-    Integer lessonProgramId;
+        List<Integer> unAssignedlessonProgramId =  response.jsonPath().getList("findAll { it.lessonProgramId}.lessonProgramId");
+        System.out.println("unAssignedlessonProgramId = " + unAssignedlessonProgramId);
+        unAssignedLessonProgId = unAssignedlessonProgramId.get(0);
+        System.out.println("unAssignedLessonProgId = " + unAssignedLessonProgId);
+    }
 
     @And("sets the Url for assigning the lesson to teacher")
     public void setsTheUrlForAssigningTheLessonToTeacher() {
@@ -43,7 +48,8 @@ public class US_12_StepDefs {
 
     @And("sets the payload for assigning the lesson to teacher")
     public void setsThePayloadForAssigningTheLessonToTeacher() {
-        payload = new Lessonpojo(lessonProgIdList,5915);
+        List<Integer> lessonProgIdList = List.of(unAssignedLessonProgId);
+        payload = new Lessonpojo(lessonProgIdList,5333);
     }
 
     @When("sends POST request and GETS response for assigning lesson")
@@ -62,48 +68,6 @@ public class US_12_StepDefs {
         LessonPostpojo actualData = response.as(LessonPostpojo.class);
         assertTrue(actualData.getMessage().contains("Lesson added to Teacher"));
         assertTrue(actualData.getHttpStatus().contains("CREATED"));
+
     }
-
-    @And("Vice Dean gets the id of the assigned lesson")
-    public void viceDeanGetsTheIdOfTheAssignedLesson() throws JsonProcessingException {
-        spec.pathParams("first","lessonPrograms", "second","getAllAssigned");
-        response = given(spec).get("{first}/{second}");
-
-        String resultList = response.asString();
-
-        List<GetAllResponsePojo> pojoList = objectMapper.readValue(resultList, new TypeReference<List<GetAllResponsePojo>>() {});
-
-        for (GetAllResponsePojo getIdLoop : pojoList)
-        {
-            if (Objects.equals(getIdLoop.getDescription(), description)) {
-                lessonProgramId = getIdLoop.getId();
-                System.out.println(lessonProgramId);
-
-            }
-        }
-
-        //List<Integer> idList = response.jsonPath().getList("findAll{it.[0].lessonName[0].lessonId=='4457'}.[0].lessonProgramId");
-        //System.out.println(idList);
-        //lessonProgramId2 = idList.get(0);
-    }
-
-    @And("Vice Dean sets the url for getting lesson by id")
-    public void viceDeanSetsTheUrlForGettingLessonById() {
-        spec.pathParams("first","lessonPrograms","second", "getById", "third", lessonProgramId);
-        response = given(spec).get("{first}/{second}/{third}");
-        response.prettyPrint();
-    }
-
-    @And("Vice Dean deletes the assigned lesson")
-    public void viceDeanDeletesTheAssignedLesson() {
-        spec.pathParams("first","lessonPrograms","second", "delete", "third", lessonProgramId);
-        response = given(spec).get("{first}/{second}/{third}");
-        //response.then().statusCode(200);
-    }
-
-    @Then("verifies status code is {int} for deleting lesson")
-    public void verifiesStatusCodeIsForDeletingLesson(int code) {
-        //assertEquals(code,response.statusCode());
-    }
-
 }
