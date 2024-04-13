befdf437cf7f38b38d195223e8976dec6296e0f7
MarielleGit <mariellematos2311@gmail.com>
2024-04-13 16:14:11 +0100

    marielle first push

--- src/test/java/baseurl/RegisterBaseUrl.java
+++ src/test/java/baseurl/RegisterBaseUrl.java
@@ -8,7 +8,7 @@ import static utilities.ManagementonSchoolAuthentication.generateToken;
 
 public class RegisterBaseUrl {
     public static RequestSpecification spec_1;
-
+// blabla
     public static void setupRegister(){
         spec_1 = new RequestSpecBuilder()
                 .setBaseUri("https://managementonschools.com/app")
